175ce81a92e8b9c8ea6558fc24d2f37f72734f8b
Nelson Kimaiga <nkimaiga@intellisoftkenya.com>
2025-05-12 22:20:10 +0300

    feat: Add StartQuestionnaire component for grantees to start the questionnaire process

---
+++ src/components/StartQuestionnaire.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StartQuestionnaire = () => {
+  return (
+    <div>
+      <h3>Start Questionnaire</h3>
+      <p>This is where the grantee can start the questionnaire.</p>
+    </div>
+  );
+};
+
+export default StartQuestionnaire;
